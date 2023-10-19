6fc221b237a0d60c35802de521520323b2ebab01
jsapro <77.3.77@mail.ru>
2023-10-19 23:49:54 +0300

    test: install jest
    install and configure jest, add test file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,18 @@
 module.exports = {
 	root: true,
-	env: { browser: true, es2020: true },
+	env: { browser: true, es2020: true, jest: true },
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:react-hooks/recommended',
 		'plugin:storybook/recommended',
+		'plugin:jest/recommended',
 		'eslint-config-prettier',
 		'prettier',
 	],
 	ignorePatterns: ['dist', '.eslintrc.cjs', '!.storybook'],
 	parser: '@typescript-eslint/parser',
-	plugins: ['react-refresh'],
+	plugins: ['react-refresh', 'jest'],
 	rules: {
 		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 	},
---
+++ jest.config.cjs
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	preset: 'ts-jest',
+	moduleNameMapper: {
+		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+			'<rootDir>/__mocks__/fileMock.js',
+		'\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+		'^@images/(.*)$': '<rootDir>/$1', // for path aliases to work
+	},
+	transform: {
+		'^.+\\.tsx?$': 'ts-jest',
+		// process `*.tsx` files with `ts-jest`
+	},
+	rootDir: 'src',
+	testEnvironment: 'jest-environment-jsdom',
+};

e65938d5719d169a45c95826434e07b6006da2ad
jsapro <77.3.77@mail.ru>
2023-10-25 15:13:03 +0300

    feat(productcard): add props with types

--- src/components/product-card/index.tsx
+++ src/components/product-card/index.tsx
@@ -2,21 +2,29 @@ import cardImage from '@images/mango.png';
 import { Link } from 'react-router-dom';
 import styles from './product-card.module.scss';
 
-const ProductCard = () => {
+type ProductCardProps = {
+	cardName: string;
+	price: string;
+	weight: string;
+	buttonText: string;
+	// добавить в пропсы cardImage когда будет готов массив карточек продуктов
+	// cardImage: string;
+};
+
+const ProductCard = ({ cardName, price, weight, buttonText }: ProductCardProps) => {
 	return (
 		<li className={styles.card}>
-			<div>
-				{/* вместо /login подставить нужный роут  */}
-				<Link className={styles.card__link} to={'/login'}>
-					<img className={styles.card__image} src={cardImage} alt="карточка товара" />
-					<h2 className={styles.card__caption}>Манго Египет</h2>
-				</Link>
-				<span className={styles.card__price}>80 руб.</span>
-				<span className={styles.card__weight}>1кг</span>
-				<div className={styles['card__button-container']}>
-					<button className={styles['card__cart-button']}>В корзину</button>
-					<button className={styles['card__like-button']} />
-				</div>
+			{/* вместо /login подставить нужный роут  */}
+			<Link className={styles.card__link} to={'/login'}>
+				<img className={styles.card__image} src={cardImage} alt="карточка товара" />
+				<h2 className={styles.card__caption}>{cardName}</h2>
+			</Link>
+			<span className={styles.card__price}>{price}</span>
+			<span className={styles.card__weight}>{weight}</span>
+			<div className={styles['card__button-container']}>
+				{/* кнопку "В корзину" заменить на кноку из UI-kit */}
+				<button className={styles['card__cart-button']}>{buttonText}</button>
+				<button className={styles['card__like-button']} />
 			</div>
 		</li>
 	);

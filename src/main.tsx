ce04282675c9663005a6a7728182573b69785301
mmariaiv <mari.ivashova@gmail.com>
2023-10-26 19:25:56 +0300

    feat: refactor ui-kit and created a directory for it
    moved ui-kit into scss/ imported these files

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.scss';
+import './scss/_variables.scss';
+import './scss/_mixins.scss';
+import './scss/base.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/auth-context.tsx';
 

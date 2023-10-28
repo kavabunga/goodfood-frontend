3a755faa3186f3cd4c4538a96843141c0ae8fe9d
jsapro <77.3.77@mail.ru>
2023-10-28 19:29:55 +0300

    fix: autofix by linter and prettier

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,20 +1,16 @@
 import styles from './button.module.scss';
 
 type ButtonProps = {
-	buttonText : string;
+	buttonText: string;
 	buttonStyle: string;
-}
+};
 
-const Button = ({buttonText, buttonStyle} : ButtonProps) => {
-
+const Button = ({ buttonText, buttonStyle }: ButtonProps) => {
 	return (
-			<button
-				type="button"
-				className={`${styles[buttonStyle]}`}
-			>
-				{buttonText}
-			</button>
-		);
-}
+		<button type="button" className={`${styles[buttonStyle]}`}>
+			{buttonText}
+		</button>
+	);
+};
 
 export default Button;

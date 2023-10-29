a76a2ad4dce017c655ea19a37ed2ad8bc51f8a09
jsapro <77.3.77@mail.ru>
2023-10-29 09:24:58 +0300

    feat(product-card.stories.tsx): add types to product card

---
+++ src/components/product-card/product-card.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
+import ProductCard from '.';
+
+const meta: Meta<typeof ProductCard> = {
+	component: ProductCard,
+	title: 'ProductCard',
+	tags: ['autodocs'],
+	argTypes: {
+		cardName: {
+			control: { type: 'text' },
+			// control: { type: 'radio' },
+			// options: [1, 2, 3],
+		},
+	},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+/**  comments shows in ProductCard story Docks  */
+export const Default: Story = {
+	args: {
+		cardName: 'Манго Египет',
+		price: '80 руб.',
+		weight: '1кг',
+		buttonText: 'В корзину',
+	},
+	render: (args) => (
+		<BrowserRouter>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					gap: '20px',
+				}}
+			>
+				<ProductCard {...args} />
+				<ProductCard {...args} />
+				<ProductCard {...args} />
+			</div>
+		</BrowserRouter>
+	),
+};

b5935ee5c37f4ea1ebcbf918b262d217c900c8d8
jsapro <77.3.77@mail.ru>
2023-10-30 12:25:47 +0300

    feat(constants.ts): add constants for Input component

---
+++ src/data/constants.ts
@@ -0,0 +1,16 @@
+export const enterEmail = 'Введите свою почту';
+export const enterPhone = 'Введите свой номер телефона';
+export const enterCard = 'Введите номер своей карты';
+export const enterPassword = 'Введите свой пароль';
+
+export const emailError =
+	'Почтовый адрес введен неверно, проверьте правильность написания';
+export const nameError = 'Имя введено неверно. Проверьте, что поле содержит кириллицу';
+export const phoneError = 'Неверный номер';
+export const cardError = 'Неверный номер карты';
+export const passwordError = 'Пароль введен неверно, попробуйте еще раз ';
+
+export const emailNotExist = 'Указанный почтовый адрес не существует, попробуйте еще раз';
+export const passwordNotSafe =
+	'Пароль небезопасен. Проверьте написание. Он должен содержать цифры, символы, буквы. Длина не менее 8 знаков';
+export const passwordNotMatch = 'Пароль не соотвествует. Проверьте написание';

bc7e4158241781e1f0cfb172ec304d77aa53432c
maik791277 <potili@bk.ru>
2023-11-01 02:09:52 +0200

    feat: add CardCatalogLink to the main page, create TopSellingThisWeek component, develop InfoCard, and adjust margins for layout alignment
    After applying this commit, the following changes will be made:
    - CardCatalogLink component will be added to the main page.
    - TopSellingThisWeek component will be created.
    - InfoCard component will be developed.
    - Margins for certain elements will be adjusted to ensure the layout aligns with the design.

---
+++ src/components/top-selling-this-week/index.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import Button from '@components/Button';
+import styles from './top-selling-this-week.module.scss';
+import ProductCard from '@components/product-card';
+import { products } from '@data/dataExamples.ts';
+
+type Product = {
+	cardName: string;
+	price: string;
+	weight: string;
+	buttonText: string;
+};
+
+const TopSellingThisWeek: React.FC = () => {
+	const [activeButton, setActiveButton] = useState<number>(1);
+
+	const handleButtonClick = (buttonId: number) => {
+		setActiveButton(buttonId);
+	};
+
+	return (
+		<div className={styles.topSellingThisWeek}>
+			<h2 className={styles.topSellingThisWeek__title}>Лидеры продаж этой недели</h2>
+			<div className={styles.topSellingThisWeek__buttonContainers}>
+				<Button
+					buttonText="Все категории"
+					buttonStyle="greenish-button"
+					classNameActive={`${activeButton === 1 ? 'greenish-button__active' : ''}`}
+					onClick={() => handleButtonClick(1)}
+				/>
+				<Button
+					buttonText="Овощи и Фрукты"
+					buttonStyle="greenish-button"
+					classNameActive={`${activeButton === 2 ? 'greenish-button__active' : ''}`}
+					onClick={() => handleButtonClick(2)}
+				/>
+				<Button
+					buttonText="Для вегетарианцев"
+					buttonStyle="greenish-button"
+					classNameActive={`${activeButton === 3 ? 'greenish-button__active' : ''}`}
+					onClick={() => handleButtonClick(3)}
+				/>
+				<Button
+					buttonText="Безлактозные продукты"
+					buttonStyle="greenish-button"
+					classNameActive={`${activeButton === 4 ? 'greenish-button__active' : ''}`}
+					onClick={() => handleButtonClick(4)}
+				/>
+			</div>
+			<div className={styles.topSellingThisWeek__cardsContainer}>
+				{products.map((product: Product, index: number) => (
+					<ProductCard
+						key={index}
+						cardName={product.cardName}
+						price={product.price}
+						weight={product.weight}
+						buttonText={product.buttonText}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default TopSellingThisWeek;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,6 +4,9 @@ import styles from './home.module.scss';
 import InfoCard from '@components/info-card';
 import usefulProductsIcon from '@images/useful-products-icon.svg';
 import roundTheClockDelivery from '@images/round-the-clock-delivery.svg';
+import CardCatalogLink from '@components/card-catalog-link';
+import { mainPageBlockLinks } from '../../data/dataExamples.ts';
+import TopSellingThisWeek from '@components/top-selling-this-week';
 
 const Home: React.FC = () => {
 	return (
@@ -24,6 +27,12 @@ const Home: React.FC = () => {
 					imageUrl={roundTheClockDelivery}
 					altText="Доставка"
 				/>
+			</section>
+			<section className={styles.home__catalogSection}>
+				<CardCatalogLink title="Каталог" type="bento-grid" array={mainPageBlockLinks} />
+			</section>
+			<section className={styles.home__topSellingThisWeek}>
+				<TopSellingThisWeek />
 			</section>
 		</div>
 	);

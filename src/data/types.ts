91c0380f2bb9345062edb3cea1d83083284f0a45
jsapro <77.3.77@mail.ru>
2023-11-02 18:26:31 +0300

    feat(types): add type to product

--- src/data/types.ts
+++ src/data/types.ts
@@ -5,3 +5,22 @@ export type ProducerType = {
 	producer_type?: string;
 	description?: string;
 };
+
+export type ProductType = {
+	name: string;
+	subcategory: number;
+	producer: number;
+	price: number;
+	components: number[];
+	kcal: number;
+	proteins: number;
+	fats: number;
+	carbohydrates: number;
+
+	tags?: number[];
+	description?: string;
+	discontinued?: boolean;
+	measure_unit?: 'grams' | 'milliliters' | 'items';
+	amount?: number;
+	views_number?: number;
+};

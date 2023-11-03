cdf0d1e413f8023c570377bb4af232326cab7a4e
jsapro <77.3.77@mail.ru>
2023-11-03 12:18:24 +0300

    fix: fix conflict after rebase

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,6 +11,79 @@ import AboutCompany from '@components/about-company/index.tsx';
 import OurBlock from '@components/our-block/our-block.tsx';
 
 const Home: React.FC = () => {
+	// const api = new Api(BACKEND_URL);
+	// api.getFavoriteProducts();
+	// api.getFavoriteProduct(777);
+	// api.getProducers();
+	// api.getProducer(333);
+	// api.postProducers({
+	// 	name: 'string',
+	// 	slug: 'string',
+	// 	producer_type: 'string',
+	// 	description: 'string',
+	// 	address: 'string',
+	// });
+	// api.patchProducer(333, {
+	// 	name: 'string',
+	// 	slug: 'string',
+	// 	producer_type: 'string',
+	// 	description: 'string',
+	// 	address: 'string',
+	// });
+	// api.deleteProducer(111);
+	// api.getProducts()
+
+	// const ggg: ProductType = {
+	// 	name: 'test-test',
+	// 	subcategory: 1,
+	// 	tags: [1],
+	// 	producer: 1,
+	// 	price: 1,
+	// 	components: [1],
+	// 	kcal: 1,
+	// 	proteins: 1,
+	// 	fats: 1,
+	// 	carbohydrates: 1,
+	// 	description: 'string',
+	// 	discontinued: true,
+	// 	measure_unit: 'grams',
+	// 	amount: 1,
+	// 	views_number: 1,
+	// };
+
+	// api.postProducts(ggg);
+
+	// api.getProduct(10)
+	// api.patchProduct(1, {
+	// 	name: 'авокадо',
+	// 	subcategory: 8,
+	// 	producer: 1,
+	// 	price: 50.0,
+	// 	components: [1],
+	// 	kcal: 100,
+	// 	proteins: 10,
+	// 	fats: 10,
+	// 	carbohydrates: 5,
+	// 	measure_unit: 'grams',
+	// });
+	// api.deleteProduct(13)
+
+	// api.postFavoriteProduct(7, {
+	// name: 'авокадо',
+	// 	subcategory: 8,
+	// 	producer: 1,
+	// 	price: 50.0,
+	// 	components: [1],
+	// 	kcal: 100,
+	// 	proteins: 10,
+	// 	fats: 10,
+	// 	carbohydrates: 5,
+	// 	measure_unit: 'grams',
+	// })
+	// api.postFavoriteProduct(21)
+
+	// api.deleteFavoriteProduct(7)
+
 	return (
 		<div>
 			<section className={styles.home__sliderSection}>

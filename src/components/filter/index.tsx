99fb74f370f5c7e47f83c4201165540a5f7b97a5
jsapro <77.3.77@mail.ru>
2023-11-22 12:15:33 +0300

    rebase: fix: linter autofix

--- src/components/filter/index.tsx
+++ src/components/filter/index.tsx
@@ -2,49 +2,53 @@ import DropDown from '@components/drop-down';
 import styles from './filter.module.scss';
 
 const Filter: React.FC = () => {
-
 	return (
 		<>
 			<div className={styles.filterDropDown}>
-
-			<DropDown />
+				<DropDown />
 			</div>
 			<div className={styles.filterBox}>
 				<ul className={styles.filterColumn}>
 					<li className={styles.filterList}>
-    				<label className={styles.filterCheck}>
+						<label className={styles.filterCheck}>
 							<input className={styles.filterCheckInput} type="checkbox" />
 							<span className={styles.filterCheckbox}></span>
-								Подходит вегетарианцам
+							Подходит вегетарианцам
 						</label>
 					</li>
 					<li className={styles.filterList}>
-    				<label className={styles.filterCheck}>
+						<label className={styles.filterCheck}>
 							<input className={styles.filterCheckInput} type="checkbox" />
 							<span className={styles.filterCheckbox}></span>
-								Без белого сахара
+							Без белого сахара
 						</label>
 					</li>
 					<li className={styles.filterList}>
-    				<label className={styles.filterCheck}>
+						<label className={styles.filterCheck}>
 							<input className={styles.filterCheckInput} type="checkbox" />
 							<span className={styles.filterCheckbox}></span>
-								Без глютена
+							Без глютена
 						</label>
 					</li>
-					<li className={`${styles['filterList']}
-							${styles['filterList_disabled']}`}>
-    				<label className={styles.filterCheck}>
-							<input className={styles.filterCheckInput} type="checkbox" disabled={true}/>
+					<li
+						className={`${styles['filterList']}
+							${styles['filterList_disabled']}`}
+					>
+						<label className={styles.filterCheck}>
+							<input
+								className={styles.filterCheckInput}
+								type="checkbox"
+								disabled={true}
+							/>
 							<span className={styles.filterCheckbox}></span>
-								Без лактозы
+							Без лактозы
 						</label>
 					</li>
 					<li className={styles.filterList}>
-    				<label className={styles.filterCheck}>
+						<label className={styles.filterCheck}>
 							<input className={styles.filterCheckInput} type="checkbox" />
 							<span className={styles.filterCheckbox}></span>
-								Для детей
+							Для детей
 						</label>
 					</li>
 				</ul>

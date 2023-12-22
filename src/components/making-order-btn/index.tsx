4a14d3f1220ab24e2001ef230709e92cdc12c237
jsapro <77.3.77@mail.ru>
2023-12-22 16:27:58 +0300

    fix: make order button disabled in case cart is empty

--- src/components/making-order-btn/index.tsx
+++ src/components/making-order-btn/index.tsx
@@ -3,13 +3,19 @@ import styles from './making-order-btn.module.scss';
 import Button from '@components/Button';
 
 interface MakingOrderBtnProps {
+	isDisabled: boolean;
 	onClick?: () => void;
 }
 
-const MakingOrderBtn: React.FC<MakingOrderBtnProps> = ({ onClick }) => {
+const MakingOrderBtn: React.FC<MakingOrderBtnProps> = ({ onClick, isDisabled }) => {
 	return (
 		<div className={styles.order}>
-			<Button buttonText="Оформить заказ" buttonStyle="green-button" onClick={onClick} />
+			<Button
+				buttonText="Оформить заказ"
+				buttonStyle="green-button"
+				onClick={onClick}
+				disabled={isDisabled}
+			/>
 			<p className={`${styles.order__title}`}>
 				Нажимая на&nbsp;кнопку &laquo;Оформить заказ&raquo;, вы&nbsp;соглашаетесь
 				с&nbsp;условиями обработки персональных данных, а&nbsp;также с&nbsp;условиями

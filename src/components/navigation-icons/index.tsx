d2fc67f4e84c8c8e51a5ae1d48d4080c436d4bd4
jsapro <77.3.77@mail.ru>
2023-12-22 19:26:19 +0300

    feat: add price near cart icon in header

--- src/components/navigation-icons/index.tsx
+++ src/components/navigation-icons/index.tsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import { useAuth } from '@hooks/use-auth.ts';
 import styles from './navigation-icons.module.scss';
 import { usePopup } from '@hooks/use-popup.ts';
+import { useCart } from '@hooks/use-cart-context';
 
 const NavigationIcons: React.FC = () => {
 	const { isLoggedIn } = useAuth();
 	const { handleOpenPopup } = usePopup();
+	const { cartData } = useCart();
 
 	return (
 		<div className={styles.navigation__icons}>
@@ -20,6 +22,7 @@ const NavigationIcons: React.FC = () => {
 					<Link
 						className={`${styles.navigation__icon} ${styles.navigation__icon_busket}`}
 						to={'/cart'}
+						data-content={`${cartData.total_price} руб.`}
 					></Link>
 					<Link
 						className={`${styles.navigation__icon} ${styles.navigation__icon_profile}`}

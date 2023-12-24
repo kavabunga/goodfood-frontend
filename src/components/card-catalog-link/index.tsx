fa76d543428e3f298a18e8c3a0dbbb59f7f78be4
jsapro <77.3.77@mail.ru>
2023-12-24 22:38:55 +0300

    refactor: delete unused code

--- src/components/card-catalog-link/index.tsx
+++ src/components/card-catalog-link/index.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable */
-import CardBlockLink from '../card-block-link';
 import clsx from 'clsx';
 import ProductCard from '../product-card';
 import styles from './card-catalog-link.module.scss';
@@ -10,7 +9,7 @@ type CardCatalogLinkProps = {
 	title: string;
 	category?: string;
 	array: Product[] | { title: string; link: string; backgroundImage: string }[];
-	type: 'bento-grid' | 'single-row';
+	type: 'single-row';
 };
 
 function CardCatalogLink({ title, array, type, category }: CardCatalogLinkProps) {
@@ -21,18 +20,14 @@ function CardCatalogLink({ title, array, type, category }: CardCatalogLinkProps)
 					styles[`card-catalog-link__title-container_type_${type}`]
 				}`}
 			>
-				<Link
-					to={`${type === 'bento-grid' ? '/catalog' : `/catalog/${category}`}`}
-					className={styles.link}
-				>
+				<Link to={`/catalog/${category}`} className={styles.link}>
 					<h2 className={styles['card-catalog-link__title']}>{title}</h2>
 				</Link>
 				<span className={styles['card-catalog-link__arrow']} />
 			</div>
 			<ul
 				className={clsx(
-					type === 'single-row' && styles['card-catalog-link__list-single'],
-					type === 'bento-grid' && styles['card-catalog-link__list-bento']
+					type === 'single-row' && styles['card-catalog-link__list-single']
 				)}
 			>
 				{array.map((item: Record<string, any>, index: number) => (
@@ -41,13 +36,6 @@ function CardCatalogLink({ title, array, type, category }: CardCatalogLinkProps)
 						key={index}
 						style={{ gridArea: item.gridArea }}
 					>
-						{type === 'bento-grid' && (
-							<CardBlockLink
-								title={item.title}
-								link={item.link}
-								backgroundImage={item.backgroundImage}
-							/>
-						)}
 						{type === 'single-row' && (
 							<ProductCard
 								cardName={item.name}

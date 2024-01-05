e2989e0b298b464b241028e5bd6314884090cc35
jsapro <77.3.77@mail.ru>
2024-01-05 11:02:48 +0300

    feat: (rebase) add button for online payment

--- src/pages/checkout/checkout-success/index.tsx
+++ src/pages/checkout/checkout-success/index.tsx
@@ -1,38 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
 
 import OurBlock from '@components/our-block';
 import OrderStatusTracker from '@components/order-status-tracker';
 import { URLS } from '@data/constants';
+import Button from '@components/button';
+import api from '@services/api';
+import { useAuth } from '@hooks/use-auth';
+import { usePopup } from '@hooks/use-popup';
 
 import styles from './checkout-success.module.scss';
 
+type Order = {
+	orderNumber: string;
+	orderId: number;
+};
+
 const CheckoutSuccess: React.FC = () => {
-	const [order, setOrder] = React.useState('');
+	const [order, setOrder] = React.useState<Order>({ orderNumber: '', orderId: 0 });
+	const [paymentUrl, setPaymentUrl] = useState('');
 	const location = useLocation();
 	const navigate = useNavigate();
+	const { isLoggedIn } = useAuth();
+	const { handleOpenPopup } = usePopup();
 
-	React.useEffect(
-		() =>
-			location.state?.order
-				? setOrder(location.state.order)
-				: navigate('/', { replace: true }),
-		[location, navigate]
-	);
+	React.useEffect(() => {
+		location.state?.orderId ? setOrder(location.state) : navigate('/', { replace: true });
+	}, [location, navigate]);
+
+	React.useEffect(() => {
+		if (order.orderId !== 0) {
+			api
+				.usersOrderPay(order.orderId)
+				.then(({ checkout_session_url }) => setPaymentUrl(checkout_session_url));
+		}
+	}, [order.orderId]);
+
+	const handlePayment = () => {
+		if (paymentUrl) return window.location.assign(paymentUrl);
+	};
 
 	return (
 		<div className={styles.checkoutSuccess}>
 			<section className={styles.checkoutSuccess__order}>
 				<h1 className={styles.checkoutSuccess__title}>
-					Заказ №{order} успешно оформлен!
+					Заказ №{order.orderNumber} успешно оформлен!
 				</h1>
+				<Button
+					onClick={handlePayment}
+					buttonText="Оплатить онлайн"
+					buttonStyle="green-button"
+				></Button>
 				<p className={styles.checkoutSuccess__paragraph}>
 					Мы уже приступили к его сборке. <br />
 					За статусом заказа можно следить в{' '}
-					<Link className={styles.checkoutSuccess__link} to={URLS.PROFILE_ORDERS}>
-						личном кабинете
-					</Link>
+					{isLoggedIn ? (
+						<Link className={styles.checkoutSuccess__link} to={URLS.PROFILE_ORDERS}>
+							личном кабинете
+						</Link>
+					) : (
+						<Link
+							onClick={() => handleOpenPopup('openPopupLogin')}
+							className={styles.checkoutSuccess__link}
+							to="/profile"
+						>
+							личном кабинете
+						</Link>
+					)}
 					.
 				</p>
 				<OrderStatusTracker />

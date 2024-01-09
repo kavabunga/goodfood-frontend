9a62b6133ec759bd92d071e9363fe91e31b6bb04
jsapro <77.3.77@mail.ru>
2024-01-09 23:28:01 +0300

    refactor: delete comments

--- src/components/scroll-to-anchor-hash/index.tsx
+++ src/components/scroll-to-anchor-hash/index.tsx
@@ -5,11 +5,9 @@ function ScrollToAnchorHash() {
 	const location = useLocation();
 	const lastHash = useRef('');
 
-	// listen to location change using useEffect with location as dependency
-	// https://jasonwatmore.com/react-router-v6-listen-to-location-route-change-without-history-listen
 	useEffect(() => {
 		if (location.hash) {
-			lastHash.current = location.hash.slice(1); // safe hash for further use after navigation
+			lastHash.current = location.hash.slice(1);
 		}
 
 		if (lastHash.current && document.getElementById(lastHash.current)) {
